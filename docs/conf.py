18e5b60c4405c5e344c36350c7596a2dd60d56c2
Yasbeck-ui <lauriezimak@hotmail.com>
2026-06-22 15:26:38 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,8 +5,8 @@ import sys
 sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
-project = 'Docs Jackson Hewitt'
-author = 'Wayans'
+project = 'Web Solutions'
+author = 'Cassie'
 release = '1.0'
 
 # -- General configuration ---------------------------------------------------
